78ff1e1faa3b72d03801aa6b43620afc7bd8af0c
Andreas Siedler <andreas.siedler@gmail.com>
2022-12-30 21:21:54 +0100

    Add next auth prisma adapter

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,16 +5,11 @@ import { PrismaClient } from "@prisma/client";
 //
 // Learn more:
 // https://pris.ly/d/help/next-js-best-practices
-
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+declare global {
+  var prisma: PrismaClient | undefined;
 }
 
-export default prisma;
+const client = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+
+export default client;
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,10 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import prisma from "../../../lib/prisma";
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID ?? "",
@@ -9,5 +12,4 @@ export default NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {},
 });
