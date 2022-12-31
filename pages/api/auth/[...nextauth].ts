ed5ec3d53a2443cdf9ff9e7799e07e7631fde565
Andreas Siedler <andreas.siedler@gmail.com>
2022-12-31 15:04:26 +0100

    Fix next-auth account update

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,47 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import { signIn } from "next-auth/react";
 import prisma from "../../../lib/prisma";
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
+  events: {
+    async signIn({ user, account }) {
+      // Update access_token and scope
+      // Workaround while prismaadapter not updating account Updates
+      const acc = await prisma.account.findFirst({
+        where: {
+          userId: user.id,
+        },
+      });
+      const updateAccount = await prisma.account.update({
+        where: {
+          id: acc?.id,
+        },
+        data: {
+          access_token: account?.access_token,
+          scope: account?.scope,
+        },
+      });
+    },
+  },
+  callbacks: {
+    async signIn(data: any) {
+      // console.log(data);
+      return true;
+    },
+  },
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
+      authorization: {
+        url: "https://github.com/login/oauth/authorize",
+        params: {
+          scope: "read:user user:email user:follow",
+        },
+      },
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
