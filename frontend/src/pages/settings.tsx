2b8e8be5d0032597f93d27808f77c64c69ddd55b
Andreas Siedler <andreas.siedler@gmail.com>
2023-02-11 21:43:30 +0100

    Update settings page

--- frontend/src/pages/settings.tsx
+++ frontend/src/pages/settings.tsx
@@ -1,56 +1,12 @@
-import { Box, Container, Divider, Flex, VStack } from "@chakra-ui/react";
-import { getSession, useSession } from "next-auth/react";
-import NextLink from "next/link";
-import { useRouter } from "next/router";
+import { getSession } from "next-auth/react";
 import { NextPageContext } from "next/types";
-import { useEffect } from "react";
-import UpdateUser from "../components/common/UpdateUser";
-import Header from "../components/layout/Header";
 import Layout from "../components/layout/Layout";
-
-const MENUS = [
-  { id: "profile", label: "Profile" },
-  { id: "account", label: "Account" },
-];
+import Settings from "../components/Settings";
 
 const SettingsPage: React.FC = () => {
-  const session = useSession();
-  const router = useRouter();
-
-  const { query } = router;
-
-  useEffect(() => {
-    if (!query.menu) router.push("/settings?menu=profile");
-  }, []);
-
   return (
     <Layout title="User settings">
-      <Container maxW={"container.lg"} px={10} py={5}>
-        <Header />
-        <Flex>
-          <VStack w={300} divider={<Divider />}>
-            {MENUS.map((menu) => (
-              <NextLink href={`/settings?menu=${menu.id}`}>
-                <Box
-                  key={menu.id}
-                  p={5}
-                  borderRadius={"lg"}
-                  bg={query.menu === menu.id ? "gray.900" : "black"}
-                  _hover={{
-                    bg: "gray.800",
-                    cursor: "pointer",
-                  }}
-                >
-                  {menu.label}
-                </Box>
-              </NextLink>
-            ))}
-          </VStack>
-          <Flex flexGrow={"1"} flexDir={"column"} px={10}>
-            {query.menu === "profile" && <UpdateUser />}
-          </Flex>
-        </Flex>
-      </Container>
+      <Settings />
     </Layout>
   );
 };
